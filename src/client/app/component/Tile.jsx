4a8e0a5e955702fcf91857649afcd59d4d9f00ff
Toby Jackson <toby@warmfusion.co.uk>
2016-09-18 20:44:18 +0100

    Simplified

--- src/client/app/component/Tile.jsx
+++ src/client/app/component/Tile.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-
-class Tile extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { client : props.client };
-  }
-
-  render () {
-    return (
-      <div className="item">
-        <h2 className="name">Name: {this.state.client.name}</h2>
-        <div>
-          <div className="status">Status: {this.state.client.output}</div>
-        </div>
-      </div>
-    );
-  }
-}
+const Tile = ({ client }) => (
+  <div className="item">
+  <h2 className="name">Name: {client.name}</h2>
+  <div>
+    <div className="status">Status: {client.output}</div>
+    </div>
+  </div>
+);
 
 export default Tile;
